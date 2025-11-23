f9da9f5f185833219e68b7f3f903e251beadb387
aidosgal <agalimzhan928@gmail.com>
2025-11-23 21:17:45 +0500

    Fix: Change the colorscheme

--- lua/bizzar/plugins/colorscheme.lua
+++ lua/bizzar/plugins/colorscheme.lua
@@ -1,31 +1,18 @@
 function ColorMyPencils(color)
-    color = color or "rose-pine-moon"
     vim.cmd.colorscheme(color)
-
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*",
+        callback = function()
+            vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+        end,
+    })
 end
 
 return {
     {
-        "ishan9299/nvim-solarized-lua",
-        name = "solarized",
+        "iruzo/matrix-nvim",
         config = function()
-            ColorMyPencils("solarized")
-        end
-    },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require('rose-pine').setup({
-                disable_background = true,
-                styles = {
-                    italic = false,
-                },
-            })
-
-            ColorMyPencils();
+            ColorMyPencils("vim")
         end
     },
 }
